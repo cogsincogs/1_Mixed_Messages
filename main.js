3e80d693ee10531601b25eb52b69fc5c6fee5b78
cogsincogs <jamiewthomas13@gmail.com>
2020-12-08 13:15:23 +0000

    Put message together and log result to console

--- main.js
+++ main.js
@@ -2,6 +2,12 @@ const colours = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink', '
 const genres = ['rock', 'pop', 'rnb', 'classical', 'jazz', 'funk', 'rap', 'blues', 'metal', 'electronica'];
 
 const getRandomNumber = max => {
-    return Math.floor(Math.random() * max) + 1;
+    return Math.floor(Math.random() * max);
 }
 
+const generateMessage = () => {
+    let message = `Your favourite colour is ${colours[getRandomNumber(colours.length)]}.\n\nYour favourite music genre is ${genres[getRandomNumber(genres.length)]}.\n\nYour favourite number is ${getRandomNumber(101)}.`
+    return message;
+}
+
+console.log(generateMessage());
