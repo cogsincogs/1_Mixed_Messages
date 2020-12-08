b0f9b2e5e06357881c2002dd6a7545da29ae0bcf
cogsincogs <jamiewthomas13@gmail.com>
2020-12-08 12:58:49 +0000

    Add arrays and random number generator

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log("Hello world!");
+const colours = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink', 'white', 'black', 'grey', 'turquoise'];
+const genres = ['rock', 'pop', 'rnb', 'classical', 'jazz', 'funk', 'rap', 'blues', 'metal', 'electronica'];
+
+const getRandomNumber = max => {
+    return Math.floor(Math.random() * max) + 1;
+}
+
